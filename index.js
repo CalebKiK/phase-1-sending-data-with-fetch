b4dffd812db809e58f70e582087cabd08074c87b
CalebKiK <karimicaleb@gmail.com>
2024-07-09 19:39:38 +0300

    Initial commit

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Add your code here
+
+function submitData(name, email){
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email,
+        }),
+    })
+
+    .then((response) => response.json())
+    .then(data => {
+        const userId = document.createElement("p");
+        userId.textContent = `New User ID: ${data.Id}`;
+
+        body.document.appendChild(userId);
+    })
+    
+    .catch(error => {
+        const errorMessage = document.createElement("p");
+        errorMessage.textContent = `Error message: ${error.message}`;
+        document.body.appendChild(errorMessage);
+    });
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+    const form = document.querySelector("form");
+    form.addEventListener("submit", (event) => {
+        event.preventDefault();
+
+        const name = document.querySelector(input[0]).value;
+        const email = document.querySelector(input[1]).value;
+        submitData(name, email);
+    });
+});
+
